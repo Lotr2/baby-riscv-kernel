a676851b3335ecb6dbe836bc3a0fb542d2e130b6
Lotr2 <mohamed_anan@aucegypt.edu>
2025-11-12 21:51:32 +0200

    debugged the run script. Seperated the compiling and linkage step to avoid error. Implemented printf function

--- src/run.sh
+++ src/run.sh
@@ -4,12 +4,13 @@ set -xue
 QEMU=qemu-system-riscv32
 
 # Path to clang and compiler flags
-CC=/mingw64/bin/clang  # Ubuntu users: use CC=clang
-CFLAGS="-std=c11 -O2 -v -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
-
+CC=/c/msys64/mingw64/bin/clang  # Ubuntu users: use CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+LDFLAGS="-m elf32lriscv -Tkernel.ld -Map=kernel.map"
 # Build the kernel
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
+$CC $CFLAGS -c -o kernel.o kernel.c
+$CC $CFLAGS -c -o common.o common.c
+ld.lld $LDFLAGS -o kernel.elf kernel.o common.o
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
