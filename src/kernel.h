64e3a69676fb05ea602ac2779bc4a9f5db3ee52b
Lotr2 <mohamed_anan@aucegypt.edu>
2025-11-20 01:59:47 +0200

    Add basic process management and context switching
    - Define process control block structure
    - Implement switch_context() for saving/restoring registers
    - Add create_process() with stack pre-population

--- src/kernel.h
+++ src/kernel.h
@@ -59,3 +59,52 @@ struct trap_frame {
         uint32_t __tmp = (value);                                              \
         __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                \
     } while (0)
+
+#define PROCS_MAX 8 
+#define PROC_UNUSED 0
+#define PROC_RUNNABLE 1
+
+struct process {
+    int pid;
+    int state;
+    vaddr_t sp;
+    uint8_t stack[8192];
+};
+
+struct process procs[PROCS_MAX];
+
+struct process *create_process(uint32_t pc){
+    struct process *proc = NULL;
+    int i;
+    for(i=0; i<PROCS_MAX;i++){
+        if(procs[i].state == PROC_UNUSED){
+            proc = &proc[i];
+            break;
+        }
+    }
+
+    if(!proc){
+        PANIC("no free process slots");
+    }
+
+    uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+    *--sp = 0;                      // s11
+    *--sp = 0;                      // s10
+    *--sp = 0;                      // s9
+    *--sp = 0;                      // s8
+    *--sp = 0;                      // s7
+    *--sp = 0;                      // s6
+    *--sp = 0;                      // s5
+    *--sp = 0;                      // s4
+    *--sp = 0;                      // s3
+    *--sp = 0;                      // s2
+    *--sp = 0;                      // s1
+    *--sp = 0;                      // s0
+    *--sp = (uint32_t) pc;          // ra
+
+    proc->pid = i+1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+
+}
